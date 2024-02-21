dcaf4a81f0f8bbdc8f146905007480724412616b
DevonGriffith <griffith.devon@gmail.com>
2024-02-20 23:12:44 -0500

    Create submit.php

---
+++ OOB_SQL_Injection/submit.php
@@ -0,0 +1,12 @@
+// Created February 20th, 2024 - By: Devon Griffith A.K.A. rootPHAGE / 我爱数据
+// Run on Windows [ php <path to file>\submit.php ]
+
+<?php
+// This is a placeholder script for demonstration purposes.
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    // In a real scenario, there would be a database query here. One is shown later.
+    echo "This is where the submitted credentials would be processed.";
+}
+?>
+
+// DO NOT USE THIS SCRIPT MALICIOUSLY - PROVIDED FOR EDUCATIONAL PURPOSES ONLY
